a4d1cc78fe5e0e382c51c4922ae2af8a575788c2
jay-tux <sys.jonas@yahoo.com>
2022-05-07 11:28:25 +0200

    Initial

---
+++ README.md
@@ -0,0 +1,6 @@
+# HMMRWAR
+
+## Used libraries
+ - [cxxopts](https://conan.io/center/cxxopts) for CLI arg parsing
+ - [seqan](https://conan.io/center/seqan) for file parsing
+ - [StochHMM](https://github.com/KorfLab/StochHMM) as the HMM implementation
---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:        main.cpp
+// Purpose:
+// Author:      jay-tux
+// Modified by:
+// Created:     May 7, 2022 11:20 AM
+// Copyright:   (c) 2022 jay-tux
+// Licence:     MPL
+/////////////////////////////////////////////////////////////////////////////
+
+int main(int argc, const char **argv) {
+  // step 1: parse cli argument
+  // step 2: signal decomposition (optionally via og hmmratac)
+  // step 3: model training using Baum-Welch (priority1)
+  // step 4: genome annotation
+}
